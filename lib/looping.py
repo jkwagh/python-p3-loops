d5389be199ba53a5c94e365d9f3dd32015ebfc46
Jeremy <kwagh.jeremiah@gmail.com>
2023-11-20 17:11:02 -0500

    submit lab assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i>0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    squared_int = [num * num for num in int_list]
+    # print(squared_int)
+    return squared_int
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(100):
+        if (i+1)%3 == 0 and (i+1)%5 ==0:
+            print("FizzBuzz")
+        elif (i+1)%3 == 0:
+            print("Fizz")
+        elif (i+1)%5 == 0:
+            print("Buzz")
+        else:
+            print(i+1)
     pass
+
+#happy_new_year()
+fizzbuzz()
